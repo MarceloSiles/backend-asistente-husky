ac34e7032533ef35b462176eef10e5f1c7a4a1a1
MarceloSiles <37555153+MarceloSiles@users.noreply.github.com>
2026-06-03 10:05:09 -0300

    Conectar reglas extra de stock y certificado

--- criticalRules.js
+++ criticalRules.js
@@ -1,6 +1,12 @@
 const { findTextRuleAnswer } = require('./textRuleSearch');
+const { findDeterministicExtraRuleAnswer } = require('./deterministicExtraRules');
 
 function findCriticalRuleAnswer(input) {
+  const extra = findDeterministicExtraRuleAnswer(input);
+  if (extra) {
+    return { id: extra.id, answer: extra.answer };
+  }
+
   const result = findTextRuleAnswer(input);
   if (result) {
     return { id: result.id, answer: result.answer };
